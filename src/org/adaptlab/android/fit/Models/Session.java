cd687aa0db800f9dbb38562c4e2d17fbf079f684
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-10-22 11:30:19 -0400

    add active record model stubs

---
+++ src/org/adaptlab/android/fit/Models/Session.java
@@ -0,0 +1,32 @@
+package org.adaptlab.android.fit.Models;
+
+import org.adaptlab.android.activerecordcloudsync.SendModel;
+import org.json.JSONObject;
+
+public class Session extends SendModel {
+
+	@Override
+	public JSONObject toJSON() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isSent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean readyToSend() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setAsSent() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
